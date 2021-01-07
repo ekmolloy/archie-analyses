33ccc0f98247bf6ff4a02e3f8d0ec2933b7c0222
ekmolloy <molloy.erin.k@gmail.com>
2021-01-07 10:55:35 -0800

    Added fake virtual environment

---
+++ scripts/export_vars.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+. /u/local/Modules/default/init/modules.sh
+module load gcc/4.9.3
+module load python/3.7.2
+module load R/3.4.0
+
+export PROJECT="archie-analyses"
+export HOME="$HOME/project-sriram/$PROJECT"
+
+export DATADIR="$SCRATCH/$PROJECT/data"
+export EXTERNALDIR="$HOME/external"
+export ARCHIESIMDIR="$EXTERNALDIR/ArchIE/simulations"
+export ARCHIEDATDIR="$EXTERNALDIR/ArchIE/data"
+export SCRIPTDIR="$HOME/scripts"
+export TOOLDIR="$HOME/tools"
+
+export PATH="$HOME:$PATH"
+export PATH="$TOOLDIR:$PATH"
+export PATH="$PYBINDIR:$PATH"
+export PATH="$EXTERNALDIR/ArchIE/msmodified:$PATH"  # ms lives here
+
+export PYTHONPATH="$HOME/.local/lib/python3.7/site-packages:$PYTHONPATH"
+export PYTHONPATH="$TOOLDIR:$PYTHONPATH"
+
